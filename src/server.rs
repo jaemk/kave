1632e42f2213858c0265f836687247601b34674a
James Kominick <james@kominick.com>
2022-04-20 20:49:45 -0400

    move client connection handling to separate function

--- src/server.rs
+++ src/server.rs
@@ -26,6 +26,14 @@ pub fn load_keys<P: AsRef<Path>>(p: P) -> Result<Vec<PrivateKey>> {
     Ok(keys)
 }
 
+macro_rules! uuid_with_ident {
+    ($name:ident) => {
+        let mut __uuid_encode_buf = uuid::Uuid::encode_buffer();
+        let $name = uuid::Uuid::new_v4();
+        let $name = $name.simple().encode_lower(&mut __uuid_encode_buf);
+    };
+}
+
 /// Server to handle client requests
 pub struct ClientServer {
     // sender for this instance to signal that it has shutdown
@@ -55,6 +63,63 @@ impl ClientServer {
     pub fn set_port(&mut self, port: u16) -> &mut Self {
         self.port = Some(port);
         self
+    }
+
+    async fn _handle_conn(
+        stream_peer_addr_res: std::result::Result<
+            (tokio::net::TcpStream, std::net::SocketAddr),
+            std::io::Error,
+        >,
+        acceptor: TlsAcceptor,
+    ) -> Result<()> {
+        uuid_with_ident!(id);
+        tracing::info!(session = id, "client connected");
+
+        let (stream, peer_addr) =
+            stream_peer_addr_res.map_err(|e| format!("session={id} error accepting tls: {e}"))?;
+        let stream = acceptor
+            .accept(stream)
+            .await
+            .map_err(|e| format!("session={id} error accepting stream: {e}"))?;
+        let (mut reader, mut writer) = split(stream);
+        let mut buf = Vec::with_capacity(1024);
+        'read: loop {
+            let n = match reader.read_buf(&mut buf).await {
+                Ok(n) => n,
+                Err(e) => {
+                    use std::io::ErrorKind::*;
+                    match e.kind() {
+                        UnexpectedEof => {
+                            tracing::debug!(session = id, "EOF on socket, disconnecting");
+                            break 'read;
+                        }
+                        _ => {
+                            return Err(
+                                format!("session={id} error reading from socket: {e}").into()
+                            )
+                        }
+                    }
+                }
+            };
+
+            tracing::debug!(
+                session = id,
+                "MESSAGE:::<{:?}>",
+                std::str::from_utf8(&buf).unwrap_or_else(|_| "unable to decode, invalid utf")
+            );
+            writer
+                .write_all(&buf)
+                .await
+                .map_err(|e| format!("session={id} error writing to socket: {e}"))?;
+            writer
+                .flush()
+                .await
+                .map_err(|e| format!("session={id} error flusing stream: {e}"))?;
+
+            tracing::debug!(session = id, "flushed {n} bytes to {peer_addr:?}");
+            buf.clear();
+        }
+        Ok(())
     }
 
     async fn _start(
@@ -93,20 +158,8 @@ impl ClientServer {
                 stream_peer_addr_res = listener.accept() => {
                     let acceptor = acceptor.clone();
                     tokio::spawn(async move {
-                        tracing::info!("CONNECTED");
-                        let (stream, peer_addr) = stream_peer_addr_res.expect("error accepting tls, todo don't panic");
-                        let stream = acceptor.accept(stream).await.expect("error accepting stream, todo don't panic");
-                        let (mut reader, mut writer) = split(stream);
-                        let mut buf = Vec::with_capacity(1024);
-                        loop {
-                            // todo: handle EOF error on client disconnects
-                            let n = reader.read_buf(&mut buf).await.expect("read error");
-                            tracing::debug!("MESSAGE::: {:?}", std::str::from_utf8(&buf).unwrap());
-                            writer.write_all(&buf).await.expect("write error");
-                            tracing::debug!("flushing");
-                            writer.flush().await.expect("error flusing, todo don't panic");
-                            tracing::debug!("flushed {n} bytes to {peer_addr:?}");
-                            buf.clear();
+                        if let Err(e) = Self::_handle_conn(stream_peer_addr_res, acceptor).await {
+                            tracing::error!("error handling client connection {e}");
                         }
                     });
                 },
