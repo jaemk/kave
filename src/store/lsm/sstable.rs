a72ba8d88b6b06a8d77e32af5b08ba2d2e5bc1d5
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2022-05-02 09:56:13 -0400

    Implement a naive SSTable that loads the whole file into memory to search

--- src/store/lsm/sstable.rs
+++ src/store/lsm/sstable.rs
@@ -1,26 +1,41 @@
 //! Sorted-string table data file format
 
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use crate::{Error, Result};
 use rb_tree::RBMap;
-use tokio::fs::{self, File, OpenOptions};
+use tokio::{
+    fs::{self, File, OpenOptions},
+    io::{AsyncReadExt, AsyncWriteExt},
+};
+
+// TODO this implementation loads the entire sstable into memory to
+// search for a value.  A better approach would be serialize the
+// sstable into an index block and a data block.  The data block is
+// all of the values from the memtable serialized and concatenated.
+// The index block is a serialized HashMap<String, u64> that maps keys
+// to offsets into the data block.  This approach means we just need
+// to load the keys into memory to search the stream instead of the
+// values as well.
 
 pub struct SSTable {
-    index_path: PathBuf,
-    data_path: PathBuf,
+    filepath: PathBuf,
 }
 
 impl SSTable {
-    pub fn new<P: Into<PathBuf>>(index_path: P, data_path: P) -> Self {
+    pub fn new<P: Into<PathBuf>>(filepath: P) -> Self {
         Self {
-            index_path: index_path.into(),
-            data_path: data_path.into(),
+            filepath: filepath.into(),
         }
     }
 
-    async fn file_handle(path: &Path) -> Result<File> {
-        match OpenOptions::new().create(true).write(true).open(path).await {
+    async fn file_handle(&self) -> Result<File> {
+        match OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(&self.filepath)
+            .await
+        {
             Ok(f) => Ok(f),
             Err(e) => Err(Error::IO(e)),
         }
@@ -28,30 +43,25 @@ impl SSTable {
 
     /// Writes the memtable to disk as an SSTable
     pub async fn write(&self, memtable: &RBMap<String, Option<Vec<u8>>>) -> Result<()> {
-        match fs::metadata(&self.index_path).await {
+        match fs::metadata(&self.filepath).await {
             Ok(_) => Err(Error::E(format!(
                 "File {} already exists",
-                &self.index_path.to_str().unwrap()
+                &self.filepath.to_str().unwrap()
             ))),
             Err(_) => Ok(()),
         }?;
-        match fs::metadata(&self.index_path).await {
-            Ok(_) => Err(Error::E(format!(
-                "File {} already exists",
-                &self.index_path.to_str().unwrap()
-            ))),
-            Err(_) => Ok(()),
-        }?;
-        let index_file = Self::file_handle(self.index_path.as_path()).await?;
-        let data_file = Self::file_handle(self.data_path.as_path()).await?;
-        for (key, val) in memtable.iter() {
-            let key = key.clone();
-            let val = val.clone();
-            // Calculate size of KV pair and offset into the data file, write that to the index file
-        }
+        let mut file = self.file_handle().await?;
+        let buf = &mut bincode::serialize(memtable)?;
+        file.write_all(buf.as_slice()).await?;
         Ok(())
     }
 
     /// Returns the value associated with the key if it exists in the SSTable.
-    pub async fn search(&self, key: String) -> Result<Option<Vec<u8>>> {}
+    pub async fn search(&self, key: String) -> Result<Option<Vec<u8>>> {
+        let mut file = self.file_handle().await?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).await?;
+        let memtable: RBMap<String, Option<Vec<u8>>> = bincode::deserialize(buf.as_slice())?;
+        Ok(memtable.get(&key).map(|v| v.to_owned()).unwrap_or(None))
+    }
 }
