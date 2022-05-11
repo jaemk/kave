bd84ad5aadf6dc2a7223e03bf426ffa53a470ab7
James Kominick <james@kominick.com>
2022-05-11 01:36:35 +0000

    refactor/split server modules

--- src/utils.rs
+++ src/utils.rs
@@ -5,3 +5,12 @@ pub fn time_since_epoch() -> Duration {
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards")
 }
+
+#[macro_export]
+macro_rules! uuid_with_ident {
+    ($name:ident) => {
+        let mut __uuid_encode_buf = uuid::Uuid::encode_buffer();
+        let $name = uuid::Uuid::new_v4();
+        let $name = $name.simple().encode_lower(&mut __uuid_encode_buf);
+    };
+}
